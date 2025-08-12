543f76d66b663372f416843c34e8a06a1e1f1832
christianlopez2 <clopez@CLOPEZL>
2025-08-11 21:38:35 -0500

    mejora rendimiento v2

--- main.py
+++ main.py
@@ -46,9 +46,10 @@ from datetime import datetime
 
 # LangChain / OpenAI / Vector
 from langchain_openai import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain.schema import Document
 
 # HTTP para SRI
